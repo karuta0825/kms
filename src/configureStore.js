e44fe5b3e40ef547ddb35a99cc0ce8cf14da9694
karuta0825 <magune3g@gmail.com>
2018-10-13 21:05:09 +0900

    fix: 初期fetch内容の追加忘れ

--- src/configureStore.js
+++ src/configureStore.js
@@ -4,6 +4,11 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import initState from './initState';
 import rootSaga from './sagas';
+import {
+  FETCH_KIDS,
+  FETCH_SERVERS,
+  FETCH_ENVIRONMENTS,
+} from './constants/ActionTypes';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -21,6 +26,8 @@ store.subscribe(() =>
 
 sagaMiddleware.run(rootSaga);
 
-store.dispatch({ type: 'FETCH_KIDS' });
+store.dispatch({ type: FETCH_KIDS });
+store.dispatch({ type: FETCH_SERVERS });
+store.dispatch({ type: FETCH_ENVIRONMENTS });
 
 export default store;
