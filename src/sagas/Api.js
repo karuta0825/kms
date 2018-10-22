0dfeed9cdb634a3ca29d31e4373f59a356472a7a
karuta0825 <magune3g@gmail.com>
2018-10-22 11:01:35 +0900

    add: HTTP GET APIの追加

--- src/sagas/Api.js
+++ src/sagas/Api.js
@@ -1,5 +1,21 @@
 // @flow
-import type { KidType } from '../types';
+import type {
+  KidType,
+  ClientType,
+  CustomerType,
+  BusivType,
+  LicenseType,
+  FenicsType,
+  HistoryType,
+  PartnerType,
+  MemoType,
+  MemoTemplateType,
+  MobileType,
+  ServerType,
+  ServiceType,
+  EventType,
+  EnvironmentType,
+} from '../types';
 
 const host = 'http://localhost:8080';
 
@@ -21,35 +37,95 @@ Api.fetchKidsByKid = (kid: number): Promise<Array<KidType>> => {
   return fetch(url, optionGet).then(r => r.json());
 };
 
-Api.fetchCustomerById = (id) => {
-
-};
-
-Api.fetchBusivById = (id) => {
-
-};
-
-Api.fetchLicenseById = (id) => {
-
-};
-
-Api.fetchFenicsById = (id) => {
-
-};
-
-Api.fetchHistoryById = (id) => {
-
-};
-
-Api.fetchServer = (): Promise<Array<Object>> => {
-  const url = `${host}/api/v1/servers`;
+Api.fetchClientById = (id: number): Promise<Array<ClientType>> => {
+  const url = `${host}/api/v1/clients/id/${id}`;
   return fetch(url, optionGet).then(r => r.json());
 };
 
-Api.fetchEnvironment = (): Promise<Array<Object>> => {
+Api.fetchBusivById = (id: number): Promise<Array<BusivType>> => {
+  const url = `${host}/api/v1/busivs/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchFenicsById = (id: number): Promise<Array<FenicsType>> => {
+  const url = `${host}/api/v1/fenics/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchCustomerById = (id: number): Promise<CustomerType> => {
+  const url = `${host}/api/v1/customers/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchLicenseById = (id: number): Promise<Array<LicenseType>> => {
+  const url = `${host}/api/v1/licenses/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchMemoById = (id: number): Promise<Array<MemoType>> => {
+  const url = `${host}/api/v1/memos/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchMobileById = (id: number): Promise<Array<MobileType>> => {
+  const url = `${host}/api/v1/mobiles/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchPartnerById = (id: number): Promise<Array<PartnerType>> => {
+  const url = `${host}/api/v1/partners/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchHistoryById = (id: number): Promise<Array<HistoryType>> => {
+  const url = `${host}/api/v1/historys/id/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchEnvironment = (): Promise<Array<ServiceType>> => {
   const url = `${host}/api/v1/environments`;
   return fetch(url, optionGet).then(r => r.json());
 };
 
+Api.fetchMemoTemplate = (): Promise<Array<MemoTemplateType>> => {
+  const url = `${host}/api/v1/memoTemplates`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchServer = (): Promise<Array<ServerType>> => {
+  const url = `${host}/api/v1/servers`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchService = (): Promise<Array<ServiceType>> => {
+  const url = `${host}/api/v1/services`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchEvent = (yearMonth: string): Promise<Array<EventType>> => {
+  const url = `${host}/api/v1/events/${yearMonth}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchAvailableUsers = (): Promise<Array<Object>> => {
+  const url = `${host}/api/v1/availableUsers`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchEnvironmentById = (id: number): Promise<Array<EnvironmentType>> => {
+  const url = `${host}/api/v1/environments/${id}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchEnvironmentByTypeAndVersion = (type: string, version: string)
+: Promise<Array<EnvironmentType>> => {
+  const url = `${host}/api/v1/environments/${type}/${version}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
+
+Api.fetchAddInfo = (yearMonth: string): Promise<Array<Object>> => {
+  const url = `${host}/api/v1/addInfo/${yearMonth}`;
+  return fetch(url, optionGet).then(r => r.json());
+};
 
 export default Api;
--- src/types/index.js
+++ src/types/index.js
@@ -77,11 +77,65 @@ export type FenicsType = {
 };
 
 export type BusivType = {
+  information: Object
+};
 
+export type LicenseType = {
+  kids_id: string,
+  K1: NumberBool,
+  U1: NumberBool,
+  U2: NumberBool,
+  U3: NumberBool,
+  U4: NumberBool,
+  U5: NumberBool,
+  U6: NumberBool,
+  U7: NumberBool,
+  U8: NumberBool,
+  U9: NumberBool,
+  UA: NumberBool,
+  UB: NumberBool,
+  UC: NumberBool,
+  UD: NumberBool,
+  UE: NumberBool,
+  UF: NumberBool,
+  UG: NumberBool,
+  UH: NumberBool,
+  UI: NumberBool,
+  C1: NumberBool,
+  C2: NumberBool,
+  C3: NumberBool,
+  C4: NumberBool,
+  S2: NumberBool,
+  S3: NumberBool,
+  S9: NumberBool,
+  SC: NumberBool,
+  SE: NumberBool,
+  SH: NumberBool,
+  SI: NumberBool,
+  SJ: NumberBool,
+  SL: NumberBool,
+  SM: NumberBool,
+  SO: NumberBool,
+  has_sd: NumberBool,
+  add_one_cli: NumberBool,
+  add_ten_cli: NumberBool,
+  add_twe_cli: NumberBool,
+  add_one_usr: NumberBool,
+  add_sd: NumberBool,
+  has_busiv: NumberBool,
 };
 
 export type MobileType = {
-
+  kids_id: string,
+  base_id: number,
+  admin_id: string,
+  admin_pw: string,
+  city_cd: string | null,
+  client_number: number,
+  disk_name: string | null,
+  disk_size: string | null,
+  fenics_key: string,
+  office_cd: string | null,
 };
 
 export type EnvironmentType = {
@@ -93,23 +147,74 @@ export type EnvironmentType = {
 };
 
 export type EventType = {
-
+  id: number,
+  date: number,
+  is_finished: NumberBool,
+  message: string,
+  start_on: Date,
+  start_on_for_view: string,
+  start_time: string,
+  start_time_for_view: string,
+  title: string,
 };
 
 export type HistoryType = {
-
+  id: number,
+  type: string,
+  content_name: string,
+  item_name: string,
+  before: string,
+  after: string,
+  create_on: Date,
+  creater: string,
+  msg: string,
 };
 
 export type MemoType = {
-
+  id: number,
+  kids_id: string,
+  title: string,
+  priority_id: number,
+  priority: string,
+  message: string,
+  short_msg: string,
+  create_on: Date,
+  name: string,
 };
 
 export type MemoTemplateType = {
-
+  id: number,
+  msg: string,
+  short_msg: string,
+  title: string,
 };
 
 export type PartnerType = {
-
+  kids_id: string,
+  em_company: string | null,
+  em_name: string | null,
+  em_email: string | null,
+  em_tel: string | null,
+  sa_pid: string | null,
+  sa_company: string | null,
+  sa_postal_cd: string | null,
+  sa_address: string | null,
+  sa_affliation: string | null,
+  sa_name: string | null,
+  sa_kana: string | null,
+  sa_email: string | null,
+  sa_tel: string | null,
+  sa_fax: string | null,
+  se_pid: string | null,
+  se_company: string | null,
+  se_postal_cd: string | null,
+  se_address: string | null,
+  se_affliation: string | null,
+  se_name: string | null,
+  se_kana: string | null,
+  se_email: string | null,
+  se_tel: string | null,
+  se_fax: string | null,
 };
 
 export type ServerType = {
@@ -125,5 +230,10 @@ export type ServerType = {
 };
 
 export type ServiceType = {
-
+  id: number,
+  is_setup_info: number,
+  sales_id: string,
+  service_id: number,
+  service_name: string,
+  version: Version,
 };
