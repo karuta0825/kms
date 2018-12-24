6431a6e2bb653dff220bf934c0dd3bff5a52ed30
karuta0825 <magune3g@gmail.com>
2018-12-24 13:47:48 +0900

    update: 入力エラーを表示させるようにする

--- src/containers/UserDetailPage/TabCustomer/InputEmail.jsx
+++ src/containers/UserDetailPage/TabCustomer/InputEmail.jsx
@@ -5,18 +5,38 @@ import TextInput from '../../../components/TextInput';
 import { changeValue } from './actions';
 
 type PropsType = {
+  title: string,
   isEdit: boolean,
   value: string,
   onChange: Event => void,
+  inputCheck: any => { isError: boolean, msg: string },
 };
 
+function inputCheck(value): { isError: boolean, msg: string } {
+  const reg = new RegExp(/^[0-9a-zA-Z-_@\.]+$/);
+
+  if (value === '') {
+    return {
+      isError: false,
+      msg: '',
+    };
+  }
+
+  return {
+    isError: !reg.test(value),
+    msg: '0-9,a-z,A-Z,@,.しか使えないよ',
+  };
+}
+
 function InputEmail(props: PropsType): React.Node {
-  return <TextInput title="Email" {...props} />;
+  return <TextInput {...props} />;
 }
 
 const mapStateToProps = state => ({
+  title: 'Email',
   isEdit: state.userDetailPage.customerTab.isEdit,
   value: state.userDetailPage.customerTab.inputValues.email,
+  inputCheck,
 });
 
 const mapDispatchToProps = dispatch => ({
