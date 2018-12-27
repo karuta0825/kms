d024db649ae9bf7c44d28dd031db4738910d52da
karuta0825 <magune3g@gmail.com>
2018-12-27 20:55:04 +0900

    add: 入力チェック専用のファイル作成

---
+++ src/utils/inputChecks.js
@@ -0,0 +1,40 @@
+// @flow
+const inputChecks = {};
+
+inputChecks.alphaNum = (value: string): boolean => {
+  const reg = new RegExp(/^[0-9a-zA-Z]+$/);
+  if (value === '') return false;
+  return !reg.test(value);
+};
+
+inputChecks.email = (value: string): boolean => {
+  const reg = new RegExp(/^[0-9a-zA-Z-_@.]+$/);
+  if (value === '') return false;
+  return !reg.test(value);
+};
+
+inputChecks.alpha = (value: string): boolean => {
+  const reg = new RegExp(/^[a-zA-Z]+$/);
+  if (value === '') return false;
+  return !reg.test(value);
+};
+
+inputChecks.upperAlpha = (value: string): boolean => {
+  const reg = new RegExp(/^[A-Z]+$/);
+  if (value === '') return false;
+  return !reg.test(value);
+};
+
+inputChecks.lowerAlpha = (value: string): boolean => {
+  const reg = new RegExp(/^[a-z]+$/);
+  if (value === '') return false;
+  return !reg.test(value);
+};
+
+inputChecks.telFax = (value: string): boolean => {
+  const reg = new RegExp(/^[0-9-]+$/);
+  if (value === '') return false;
+  return !reg.test(value);
+};
+
+export default inputChecks;
