2ceb2030f908d2b2ee524ccaf4e02c5d49f44eda
karuta0825 <magune3g@gmail.com>
2018-12-27 20:06:43 +0900

    update: eslint対応と一覧に戻ったときに参照モードに戻すように対応

--- src/containers/UserDetailPage/index.jsx
+++ src/containers/UserDetailPage/index.jsx
@@ -1,19 +1,26 @@
+// @flow
 import * as React from 'react';
 import { connect } from 'react-redux';
 import styles from './css/index.css';
 import UserInfo from './UserInfo';
 import Memo from './Memo';
-import { getUserInfo } from './actions';
+import { getUserInfo, toggleEditMode } from './actions';
 
 type PropsType = {
   match: Object,
-  dispatch: () => void,
+  dispatch: any => void,
 };
 
 class UserDetail extends React.Component<PropsType> {
   componentWillMount() {
     const { match, dispatch } = this.props;
     dispatch(getUserInfo(match.params.id));
+  }
+
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch(toggleEditMode(false, 'BASEINFO'));
+    dispatch(toggleEditMode(false, 'CUSTOMER'));
   }
 
   render(): React.Node {
