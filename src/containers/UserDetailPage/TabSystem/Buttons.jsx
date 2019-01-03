16647e3e4a4f868036399c35ef60551a9a5e81a8
karuta0825 <magune3g@gmail.com>
2019-01-03 16:37:09 +0900

    update: Actionをひとつのコンポーネントにまとめる

--- src/containers/UserDetailPage/TabSystem/Buttons.jsx
+++ src/containers/UserDetailPage/TabSystem/Buttons.jsx
@@ -1,12 +1,58 @@
 // @flow
 import * as React from 'react';
 import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+import { toggleEditMode } from '../actions';
 
-const Buttons = (props) => {
-  const { isEdit } = props;
+type PropsType = {
+  isEdit: boolean,
+  onClickSave: Event => void,
+  onClickCancel: Event => void,
+  onClickEdit: Event => void,
+};
+
+const Buttons = (props: PropsType): React.Node => {
+  const {
+    isEdit,
+    onClickSave,
+    onClickCancel,
+    onClickEdit,
+  } = props;
   return (
-    { isEdit && <Button variant="contained">保存</Button> }
-    { !isEdit && <Button variant="contained" color="secondary">編集</Button> }
-    { isEdit && <Button variant="contained">キャンセル</Button> }
+    <React.Fragment>
+      {isEdit && <Button variant="contained">保存</Button>}
+      {isEdit && (
+        <Button variant="contained" onClick={onClickCancel}>
+          キャンセル
+        </Button>
+      )}
+      {!isEdit && (
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={onClickEdit}
+        >
+          編集
+        </Button>
+      )}
+    </React.Fragment>
   );
-}
+};
+
+const mapStateToProps = (state: StateType) => ({
+  isEdit: state.userDetailPage.baseInfoTab.isEdit,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onClickCancel: () => {
+    dispatch(toggleEditMode(false, 'BASEINFO'));
+  },
+  onClickEdit: () => {
+    dispatch(toggleEditMode(true, 'BASEINFO'));
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Buttons);
--- src/containers/UserDetailPage/TabSystem/index.jsx
+++ src/containers/UserDetailPage/TabSystem/index.jsx
@@ -1,9 +1,7 @@
 // @flow
 import * as React from 'react';
 import { connect } from 'react-redux';
-import Button from '@material-ui/core/Button';
 import styles from '../css/tab.css';
-import { toggleEditMode } from '../actions';
 import InputKid from './InputKid';
 import SelectAPServer from './SelectAPServer';
 import InputStartDate from './InputStartDate';
@@ -20,41 +18,18 @@ import InputFenicsNum from './InputFenicsNum';
 import SelectNetwork from './SelectNetwork';
 import SelectVersion from './SelectVersion';
 import SelectSystemType from './SelectSystemType';
+import Buttons from './Buttons';
 
 type PropsType = {
-  isEdit: boolean,
   has_busiv: boolean,
-  onClickSave: Event => void,
-  onClickCancel: Event => void,
-  onClickEdit: Event => void,
 };
 
 function TabSystem(props: PropsType): React.Node {
-  const {
-    isEdit,
-    has_busiv,
-    onClickSave,
-    onClickCancel,
-    onClickEdit,
-  } = props;
+  const { has_busiv } = props;
   return (
     <div className={styles.wrapper}>
       <div className={styles.action}>
-        {isEdit && <Button variant="contained">保存</Button>}
-        {isEdit && (
-          <Button variant="contained" onClick={onClickCancel}>
-            キャンセル
-          </Button>
-        )}
-        {!isEdit && (
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={onClickEdit}
-          >
-            編集
-          </Button>
-        )}
+        <Buttons />
       </div>
       <div className={styles.body}>
         <InputStartDate />
@@ -79,21 +54,8 @@ function TabSystem(props: PropsType): React.Node {
 }
 
 const mapStateToProps = (state: StateType) => ({
-  isEdit: state.userDetailPage.baseInfoTab.isEdit,
   has_busiv:
     state.userDetailPage.baseInfoTab.inputValues.has_busiv === 1,
 });
 
-const mapDispatchToProps = dispatch => ({
-  onClickCancel: () => {
-    dispatch(toggleEditMode(false, 'BASEINFO'));
-  },
-  onClickEdit: () => {
-    dispatch(toggleEditMode(true, 'BASEINFO'));
-  },
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TabSystem);
+export default connect(mapStateToProps)(TabSystem);
