194a8ff0972dc2dcac852bf3263730123840d249
karuta0825 <magune3g@gmail.com>
2019-01-07 23:03:32 +0900

    update: Switchコンポーネントを変更する

--- src/containers/UserDetailPage/TabCustomer/SwitchBusiv.jsx
+++ src/containers/UserDetailPage/TabCustomer/SwitchBusiv.jsx
@@ -1,28 +1,53 @@
 // @flow
 import * as React from 'react';
 import { connect } from 'react-redux';
-import SwitchControl from '../../../components/SwitchControl';
+import Switch from '../../../components/Switch';
+import styles from '../../../components/css/TextInput.css';
 import { changeValue } from './actions';
 
-function SwitchBusiv(props): React.Node {
-  return <SwitchControl {...props} />;
+type PropsType = {
+  title: string,
+  isEdit: boolean,
+  isSelected: boolean,
+  onClick: Event => void,
+};
+
+function SwitchBusiv(props: PropsType): React.Node {
+  const { title, isEdit, isSelected, onClick } = props;
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.title}>{title}</span>
+      <Switch
+        name="あり"
+        isEdit={isEdit}
+        isSelected={isSelected}
+        onClick={onClick}
+      />
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
   title: 'ビジネスVPN',
   isEdit: state.userDetailPage.customerTab.isEdit,
-  isChecked:
+  isSelected:
     state.userDetailPage.customerTab.inputValues.has_busiv === 1,
 });
 
-const mapDispatchToProps = dispatch => ({
-  onChange: e => {
-    const value = e.target.checked ? 1 : 0;
-    dispatch(changeValue('has_busiv', value));
-  },
-});
+const mergeProps = (stateProps, dispatchProps): PropsType => {
+  const { isSelected } = stateProps;
+  const { dispatch } = dispatchProps;
+  return {
+    ...stateProps,
+    onClick: () => {
+      const value = isSelected ? 0 : 1;
+      dispatch(changeValue('has_busiv', value));
+    },
+  };
+};
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  null,
+  mergeProps
 )(SwitchBusiv);
--- src/containers/UserDetailPage/TabCustomer/SwitchFenics.jsx
+++ src/containers/UserDetailPage/TabCustomer/SwitchFenics.jsx
@@ -1,29 +1,54 @@
 // @flow
 import * as React from 'react';
 import { connect } from 'react-redux';
-import SwitchControl from '../../../components/SwitchControl';
+import Switch from '../../../components/Switch';
+import styles from '../../../components/css/TextInput.css';
 import { changeValue } from './actions';
 
-function SwitchFenics(props): React.Node {
-  return <SwitchControl {...props} />;
+type PropsType = {
+  title: string,
+  isEdit: boolean,
+  isSelected: boolean,
+  onClick: Event => void,
+};
+
+function SwitchFenics(props: PropsType): React.Node {
+  const { title, isEdit, isSelected, onClick } = props;
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.title}>{title}</span>
+      <Switch
+        name="あり"
+        isEdit={isEdit}
+        isSelected={isSelected}
+        onClick={onClick}
+      />
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
   title: 'Fenics',
   isEdit: state.userDetailPage.customerTab.isEdit,
-  isChecked:
+  isSelected:
     state.userDetailPage.customerTab.inputValues.has_fenics ===
     1,
 });
 
-const mapDispatchToProps = dispatch => ({
-  onChange: e => {
-    const value = e.target.checked ? 1 : 0;
-    dispatch(changeValue('has_fenics', value));
-  },
-});
+const mergeProps = (stateProps, dispatchProps): PropsType => {
+  const { isSelected } = stateProps;
+  const { dispatch } = dispatchProps;
+  return {
+    ...stateProps,
+    onClick: () => {
+      const value = isSelected ? 0 : 1;
+      dispatch(changeValue('has_fenics', value));
+    },
+  };
+};
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  null,
+  mergeProps
 )(SwitchFenics);
