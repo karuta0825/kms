0649123b57fe66880d0a9af200a1b7b03d9d1704
karuta0825 <magune3g@gmail.com>
2019-01-12 20:04:33 +0900

    update: QA情報のコンポーネントを変更

--- src/containers/UserDetailPage/TabSystem/SwitchQA.jsx
+++ src/containers/UserDetailPage/TabSystem/SwitchQA.jsx
@@ -1,28 +1,50 @@
 // @flow
 import * as React from 'react';
 import { connect } from 'react-redux';
-import SwitchControl from '../../../components/SwitchControl';
-import { changeValue } from './actions';
+import Switch from '../../../components/Switch';
+import styles from '../../../components/css/TextInput.css';
 
-function SwitchQA(props): React.Node {
-  return <SwitchControl {...props} />;
+type PropsType = {
+  title: string,
+  isEdit: boolean,
+  isSelected: boolean,
+};
+
+function helperText(isEdit: boolean): React.Node | null {
+  const styles = {
+    fontSize: '10px',
+    marginTop: '10px',
+  };
+  if (isEdit) {
+    return (
+      <span style={styles}>
+        顧客情報タブから変更してください
+      </span>
+    );
+  }
+  return null;
+}
+
+function SwitchQA(props: PropsType): React.Node {
+  const { title, isEdit, isSelected } = props;
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.title}>{title}</span>
+      <Switch
+        name="あり"
+        isEdit={false}
+        isSelected={isSelected}
+      />
+      {helperText(isEdit)}
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-  title: '業務QA',
+  title: '業務QA(全拠点中1つでも)',
   isEdit: state.userDetailPage.baseInfoTab.isEdit,
-  isChecked:
+  isSelected:
     state.userDetailPage.baseInfoTab.inputValues.has_qa === 1,
 });
 
-const mapDispatchToProps = dispatch => ({
-  onChange: e => {
-    const value = e.target.checked ? 1 : 0;
-    dispatch(changeValue('has_qa', value));
-  },
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(SwitchQA);
+export default connect(mapStateToProps)(SwitchQA);
