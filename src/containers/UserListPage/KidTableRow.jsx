d91c77555989e61296a39ce8954f24605f7226af
karuta0825 <magune3g@gmail.com>
2019-02-23 13:13:20 +0900

    update: ホバー時に色変化させる

--- src/containers/UserListPage/KidTableRow.jsx
+++ src/containers/UserListPage/KidTableRow.jsx
@@ -3,6 +3,14 @@ import * as React from 'react';
 import { Table } from '@devexpress/dx-react-grid-material-ui';
 import styles from './css/table.css';
 
-export default (props): React.Node => (
+type PropsType = {
+  key: string,
+  type: Symbol,
+  rowId?: number | string,
+  row?: any,
+  height?: number,
+};
+
+export default (props: PropsType): React.Node => (
   <Table.Row {...props} className={styles.row} />
 );
