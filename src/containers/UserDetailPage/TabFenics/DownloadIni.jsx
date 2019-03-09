fce1492953ab4fd17521ad60146fdbdd846d47c2
karuta0825 <magune3g@gmail.com>
2019-03-09 11:13:51 +0900

    fix: tableのselectionを変更したことへの影響調査対応

--- src/containers/UserDetailPage/TabFenics/DownloadIni.jsx
+++ src/containers/UserDetailPage/TabFenics/DownloadIni.jsx
@@ -9,21 +9,19 @@ function makeBlob(state: StateType): Blob {
   const header =
     '"※update_flag[A:Add,M:modify,D:Delete] ",※Prefix,※user_label,init_password,id_group,access_control_group,start_date[ex.20091201],end_date[ex.20091231],comment1,comment2,comment3,"regist_terminal_flag[1:Pre-Reg,2:Auto-Reg,3:Approval]",card_line_no01[ex.09012345678],card_line_no02[ex.09012345678],card_line_no03[ex.09012345678],card_line_no04[ex.09012345678],card_line_no05[ex.09012345678],card_line_no06[ex.09012345678],card_line_no07[ex.09012345678],card_line_no08[ex.09012345678],card_line_no09[ex.09012345678],card_line_no10[ex.09012345678]';
   const { selection } = state.userDetailPage.fenicsTab;
-  const onlyPCFenicsData = state.data.fenics.filter(
-    (item: FenicsType) => item.is_mobile === 0
+  const selectionItem = selection.map(item =>
+    state.data.fenics.find(fenics => fenics.fenics_id === item)
   );
-  const selectionData = selection.map(num => ({
+  const selectionData = selectionItem.map((fenics: FenicsType) => ({
     update_flag: 'A',
     prefix: 'hopecl-',
-    user_label: onlyPCFenicsData[num].fenics_id,
-    init_password: onlyPCFenicsData[num].password,
+    user_label: fenics.fenics_id,
+    init_password: fenics.password,
     id_group: 'お客様　システム１',
     access_control_group: 'お客様用　システム１',
     start_date:
-      (onlyPCFenicsData[num].start_on &&
-        moment(onlyPCFenicsData[num].start_on).format(
-          'YYYYMMDD'
-        )) ||
+      (fenics.start_on &&
+        moment(fenics.start_on).format('YYYYMMDD')) ||
       '',
     end_date: '',
     comment1: state.data.baseInfo.user_name,
