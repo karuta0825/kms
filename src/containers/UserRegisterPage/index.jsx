54fd19c4bfc9b3561f01079d63480f4c59b41d27
karuta0825 <magune3g@gmail.com>
2019-03-12 07:46:12 +0900

    add: 登録用関数作成

--- src/containers/UserRegisterPage/index.jsx
+++ src/containers/UserRegisterPage/index.jsx
@@ -11,15 +11,89 @@ type sentence = string;
 type PropsType = {
   canUploadFile: boolean,
   onDrop: (SyntheticDragEvent<any>) => void,
-  onClick: (Event) => void,
+  onClick: Event => void,
 };
-
 
 function getLineList(fileReader: FileReader): Array<sentence> {
   const { result } = fileReader;
   const lineCode = result.indexOf('\r') === -1 ? '\n' : '\r\n';
   const lines = result.split(lineCode);
-  return lines;
+  return lines.filter(line => line !== '' && line.indexOf('#') !== 0);
+}
+
+function setLicenseInfo(obj, services, version) {
+  const replace = {};
+  const { kids, licenses, customers, ...rest } = obj;
+  const clients = { number: 1 };
+  const mobiles = { number: 1 };
+  kids.number_pc = 1;
+  Object.keys(licenses).forEach(key => {
+    const item = services.find(
+      service =>
+        service.sales_id === key && service.version === version
+    );
+    if (item && item.is_setup_info !== 1) {
+      replace[item.service_id] = licenses[key];
+    } else {
+      // ネットワーク
+      if (item && item.service_id === 'has_busiv') {
+        customers.has_busiv = licenses[key] === 1 ? 1 : 0;
+        customers.has_fenics = licenses[key] === 1 ? 0 : 1;
+      }
+
+      // モバイル使用
+      if (item && item.service_id === 'has_sd') {
+        replace.K1 = 1;
+        customers.has_mobile = licenses[key];
+      }
+
+      // 追加1クライアント数
+      if (item && item.service_id === 'add_one_cli') {
+        kids.number_pc += Number(licenses[key]);
+      }
+
+      // 追加10クライアント数
+      if (item && item.service_id === 'add_ten_cli') {
+        kids.number_pc += 10 * Number(licenses[key]);
+      }
+
+      // 追加20クライアント数
+      if (item && item.service_id === 'add_twe_cli') {
+        kids.number_pc += 20 * Number(licenses[key]);
+      }
+
+      // 追加ユーザー
+      if (item && item.service_id === 'add_one_usr') {
+        clients.number += licenses[key];
+      }
+
+      if (item && item.service_id === 'add_sd') {
+        mobiles.number = licenses[key];
+      }
+    }
+  });
+  console.log(obj);
+  console.log({
+    kids,
+    clients,
+    mobiles,
+    customers,
+    ...rest,
+    licenses: replace,
+  });
+}
+
+function makeUploadData(lines: Array<sentence>): Object {
+  const obj = {};
+  lines.forEach(line => {
+    const delimiterPosition = line.indexOf(':');
+    const key = line.slice(0, delimiterPosition);
+    const value = line.slice(delimiterPosition + 1);
+    const [table, field] = key.split('__');
+    obj[table] = obj[table] || {};
+    obj[table][field.trim()] = value.trim();
+  });
+  return obj;
 }
 
 function UserRegister(props: PropsType): React.Node {
@@ -55,11 +129,12 @@ function UserRegister(props: PropsType): React.Node {
   );
 }
 
-const mapStateToProps = state => ({
-  canUploadFile: state.userRegisterPage.canUploadFile,
-});
+const mapStateToProps = state => state;
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({ dispatch });
+
+const mergeProps = (state: StateType, { dispatch }) => ({
+  canUploadFile: state.userRegisterPage.canUploadFile,
   onDrop: (e: SyntheticDragEvent<any>) => {
     e.stopPropagation();
     e.preventDefault();
@@ -68,7 +143,26 @@ const mapDispatchToProps = dispatch => ({
     r.readAsText(file);
     r.onload = () => {
       const lines = getLineList(r);
-      // linesに対して入力チェックで、dispatchの分岐
+      const [kid, ...last] = lines;
+
+      // 存在しないKIDは登録できない
+      const [user] = state.data.kids.filter(r => r.kid === kid);
+      if (!user || !user.kid) {
+        console.log('存在しないぞ');
+      }
+
+      // 上書き登録はできない
+      if (user.is_registered === 1) {
+        console.log('二度目の登録はできません');
+      }
+
+      const prepareObj = makeUploadData(last);
+      const obj = setLicenseInfo(
+        prepareObj,
+        state.data.services,
+        user.version
+      );
+
       dispatch(canUpload(true));
     };
   },
@@ -80,4 +174,5 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
+  mergeProps
 )(UserRegister);
